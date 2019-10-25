0a865133673513c7468deb7d250ee99bc4504090
Gary Henderson <hendergy@gmail.com>
2019-10-25 19:13:24 -0400

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to this test site.</p>
+    <p>We aren't building anything here. Just testing. Like GlaDOS.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
